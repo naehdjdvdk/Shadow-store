3811de81fe390c1daa38b68d2d9812b09a68a619
Julia <juliahanna660@gmail.com>
2025-04-26 04:17:28 +0300

    Create products.js

---
+++ products.js
@@ -0,0 +1,26 @@
+const products = [
+  {
+    id: 1,
+    name: "سماعة بلوتوث",
+    price: 99,
+    image: "https://via.placeholder.com/200x150?text=سماعة"
+  },
+  {
+    id: 2,
+    name: "ساعة ذكية",
+    price: 149,
+    image: "https://via.placeholder.com/200x150?text=ساعة"
+  },
+  {
+    id: 3,
+    name: "شاحن سريع",
+    price: 39,
+    image: "https://via.placeholder.com/200x150?text=شاحن"
+  },
+  {
+    id: 4,
+    name: "كيبورد ميكانيكي",
+    price: 199,
+    image: "https://via.placeholder.com/200x150?text=كيبورد"
+  }
+];
